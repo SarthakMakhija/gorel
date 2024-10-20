2f41b7d1d35711afb4865df20f8290cee01c852e
Sarthak <sarthak.makhija@gmail.com>
2024-10-20 21:35:26 +0530

    Sarthak | Adds support for encoding and decoding uint16, uint32, uint64 in encoder_decoder

--- encoder_decoder.go
+++ encoder_decoder.go
@@ -23,3 +23,27 @@ func DecodeByteSlice(source []byte, fromOffset uint16) []byte {
 	endOffset := fromOffset + uint16(reservedSizeForByteSlice) + byteSliceLength
 	return source[fromOffset+uint16(reservedSizeForByteSlice) : endOffset]
 }
+
+func EncodeUint16(source uint16, destination []byte, destinationStartingOffset uint) {
+	binary.LittleEndian.PutUint16(destination[destinationStartingOffset:], source)
+}
+
+func DecodeUint16(source []byte, fromOffset uint16) uint16 {
+	return binary.LittleEndian.Uint16(source[fromOffset:])
+}
+
+func EncodeUint32(source uint32, destination []byte, destinationStartingOffset uint) {
+	binary.LittleEndian.PutUint32(destination[destinationStartingOffset:], source)
+}
+
+func DecodeUint32(source []byte, fromOffset uint16) uint32 {
+	return binary.LittleEndian.Uint32(source[fromOffset:])
+}
+
+func EncodeUint64(source uint64, destination []byte, destinationStartingOffset uint) {
+	binary.LittleEndian.PutUint64(destination[destinationStartingOffset:], source)
+}
+
+func DecodeUint64(source []byte, fromOffset uint16) uint64 {
+	return binary.LittleEndian.Uint64(source[fromOffset:])
+}
--- encoder_decoder_test.go
+++ encoder_decoder_test.go
@@ -16,3 +16,27 @@ func TestEncodeAndDecodeAByteSlice(t *testing.T) {
 	sourceBytes := DecodeByteSlice(destination[:numberOfBytesForEncoding], 0)
 	assert.Equal(t, "LSM stands for log-structured merge tree", string(sourceBytes))
 }
+
+func TestEncodeAndDecodeAnUint16(t *testing.T) {
+	value := uint16(100)
+	destination := make([]byte, 2)
+
+	EncodeUint16(value, destination, 0)
+	assert.Equal(t, uint16(100), DecodeUint16(destination, 0))
+}
+
+func TestEncodeAndDecodeAnUint32(t *testing.T) {
+	value := uint32(5400)
+	destination := make([]byte, 4)
+
+	EncodeUint32(value, destination, 0)
+	assert.Equal(t, uint32(5400), DecodeUint32(destination, 0))
+}
+
+func TestEncodeAndDecodeAnUint64(t *testing.T) {
+	value := uint64(10000)
+	destination := make([]byte, 8)
+
+	EncodeUint64(value, destination, 0)
+	assert.Equal(t, uint64(10000), DecodeUint64(destination, 0))
+}
